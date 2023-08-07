e3005df78fdc943dde935a94c6ee4a4a70b96faf
cesxes <mottacesar001@gmail.com>
2023-08-07 02:48:36 -0300

    GET NAME AND REVIEWS BY LOGED USER

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom' 
 import LoginPageClient from './pages/LoginClientPage'
+import HomePage from './pages/HomePage'
 function App() {
   return (
     <div >
       <Router>
          <Routes> 
            <Route path ='/' element={<LoginPageClient/>}></Route>
-
-           </Routes>
+           <Route path ="home" element ={<HomePage/> }></Route>
+          </Routes>
       </Router>
     </div>
   )
--- src/pages/LoginClientPage.js
+++ src/pages/LoginClientPage.js
@@ -1,17 +1,38 @@
 import styles from '../styles/LoginClient.module.css'
 import React from 'react'
+import axios  from 'axios';
+import {useState} from 'react'; 
+
 function LoginPage() {
+
+  const [email, setEmail] = useState('');
+  const [pass, setPassword] = useState('');
+  const HandleEmail = (e) => {setEmail(e.target.value)};
+  const HandlePass = (e) => {setPassword(e.target.value)};
+  const SendFormValues = async (e) => { e.preventDefault(); 
+  try {
+      const response = await axios.post('login', { email, pass })
+       if(response.date = true) {
+        window.location.assign("http://localhost:3000/home");
+       }
+    } catch (error) {
+      console.error('Erro durante a requisição:', error.message);
+    }
+  };
+
+  
+
  return (
    <div className={styles.Container}>
        <h3> Opine aqui! </h3>
       <div className={styles.BoxLogin}>
         <div className={styles.FormLogin}>
-        <form>
-            <input type='email' placeholder = "EMAIL"></input>
+        <form onSubmit ={SendFormValues} >
+            <input type='email' onChange = {HandleEmail} placeholder = "EMAIL"></input>
             <br/> <br/>
-            <input type='password' placeholder = "SENHA"></input>
+            <input type='password' onChange = {HandlePass} placeholder = "SENHA"></input>
             <br/> <br/>
-            <input type="submit" value="Enviar" />  
+            <input type="submit" value="Entrar" />  
 
          </form>
           <br/> <br/> 
